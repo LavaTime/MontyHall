acbc165910354abd2eda08031639997b0f6f6a2a
Amit Keren <amitkeren9@gmail.com>
2019-12-26 16:50:00 +0200

    started adding the random mechanism for the random door shuffling doors and added a line to test positions
    CHANGELOG:
    
    1. imported random library.
    2. moved the location of the Class Obj.
    3. added door var = order of the doors. will contain a tuple of booleans.
    4. made all of the objs to have a Rect property.
    5. added testing for mouse clicking of all 3 doors.
    
    TODO:
    Write actual code for the doors and button presses

--- Display.py
+++ Display.py
@@ -4,20 +4,12 @@ from typing import List, Any
 import pygame as pg
 import time
 import numpy as np
+import random
 from pygame.locals import *
 
 # Initiating functions
 pg.init()
 pg.display.init()
-
-# Defining constants
-WIDTH, HEIGHT = (1280, 720)
-SIZE = (WIDTH, HEIGHT)
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
 
 
 class Obj:
@@ -36,6 +28,7 @@ class Obj:
         self.y = ypos
         self.texture = currenttexture
         self.visible = visible
+        self.Rect = pg.Rect((xpos, ypos), (229, 394))
         Obj.List.append(self)
 
     List = []
@@ -44,6 +37,14 @@ class Obj:
         # display the object on the screen on the corresponding positions and texture
         screen.blit(self.texture, (self.x, self.y))
 
+# Defining constants
+WIDTH, HEIGHT = (1280, 720)
+SIZE = (WIDTH, HEIGHT)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
 
 # Screen settings
 screen = pg.display.set_mode(size=SIZE)
@@ -62,6 +63,8 @@ door2 = Obj(550, 30, closedDoorImg, True)
 door3 = Obj(959, 30, closedDoorImg, True)
 chosenDoor = False
 
+doors = random.shuffle
+
 while True:
     screen.fill(WHITE)
     for i in Obj.List:
@@ -71,6 +74,7 @@ while True:
     dontSwitchButton = pg.Rect(805, 500, 400, 150)
     pg.draw.rect(screen, GREEN, switchButton)
     pg.draw.rect(screen, RED, dontSwitchButton)
+    pg.draw.line(screen, BLACK, (0, 0), (150, 30))
     pg.display.flip()
     for event in pg.event.get():
         # Check if the mouse is clicked down and it's position
@@ -85,7 +89,16 @@ while True:
                     print("Don't switch button pressed")
                 else:
                     pass
-
+            else:
+                # TODO: Write buttons
+                if pg.Rect.collidepoint(door1.Rect, mousePos):
+                    print("door 1 pressed")
+                elif pg.Rect.collidepoint(door2.Rect, mousePos):
+                    print("door 2 pressed")
+                elif pg.Rect.collidepoint(door3.Rect, mousePos):
+                    print("door 3 pressed")
+                else:
+                    pass
 
         # check if the event is the X button
         if event.type == pg.QUIT:
